be34bad079c100c0e1f757387366985e433e0ff1
Aman Hussain <ammu622400@gmail.com>
2026-06-29 02:06:46 +0530

    Added razorpay package

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='orders')
+
+router.register(r'', OrderViewSet, basename='orders')
 
 urlpatterns = [
     path('', include(router.urls)),
