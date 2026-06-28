dc056eaab9f2689dc0a200f3a89b45e44d60d884
Aman Hussain <ammu622400@gmail.com>
2026-06-29 01:49:13 +0530

    Added razorpay package

--- payments/urls.py
+++ payments/urls.py
@@ -1,21 +1,16 @@
-# payments/urls.py
-
 from django.urls import path
-
-from .views import (
-    create_payment_order,
-    verify_payment
-)
+from .views import create_payment_order, verify_payment
 
 urlpatterns = [
-
     path(
         "create-order/",
-        create_payment_order
+        create_payment_order,
+        name="create-order"
     ),
 
     path(
         "verify/",
-        verify_payment
+        verify_payment,
+        name="verify-payment"
     ),
 ]
